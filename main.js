d8e85442eab0093cdd5be5eae9b9ebe6241cd112
Maria <mmorrison1978@gmail.com>
2016-06-19 13:16:45 -0600

    changed dataType to jsonp hopefully to fix cors issuej

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ function getWeather() {
  var retrieveData = $.ajax({
   url: 'http://api.openweathermap.org/data/2.5/weather?lat=39.7003055&lon=-104.8917007&units=metric&appid=4d6648a4bbb807b27dc7b11eb91cd4d9',
   type: 'GET',
-  dataType: 'JSONP'
+  dataType: 'jsonp'
  });
+
 
  retrieveData.done(function(data) {
   console.log(data);
